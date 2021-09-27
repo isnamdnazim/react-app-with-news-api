abd6d857789da83bb3c06b09c2bb2fe10df9f22f
isnamdnazim <isnamdnazim@gmail.com>
2021-09-27 23:36:52 +0600

    start working with news api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [news, setNews] = useState([])
+  console.log(news)
+
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-27&sortBy=publishedAt&apiKey=4656a5f121bd4c0a8eb39fea36624704')
+      .then(res => res.json())
+      .then(data => setNews(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
